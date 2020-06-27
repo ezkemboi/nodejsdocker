37748bcf32ddc5b5e158024e87f49669c88af739
ezkemboi <ezrqnkemboi@gmail.com>
2020-06-27 12:39:18 +0300

    build mpesa and validator skeleton

--- src/mpesa/index.ts
+++ src/mpesa/index.ts
@@ -1,5 +1,25 @@
 // PORT 8001
+
+const validMpesaRes = [
+    {
+        mpesaCode: 'NXQUIOAKSDII12JS',
+        mpesaUser: 'kemboi',
+        phoneNumber: '0722228902',
+        success: true,
+        amount: '5000',
+        message: 'Successfully paid 5000 for rent'
+    },
+    {
+        mpesaUser: 'Kebenei',
+        phoneNumber: '0798862345',
+        success: false,
+        amount: '5000',
+        message: 'Failed. Insufficient funds.'
+    }
+]
+
 export function mpesa() {
     // process payment
     // response -> send back to queue
+    console.log({ validMpesaRes })
 }
--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,10 +1,30 @@
 // Client -> send request to PORT -> 8000
 // 8000 -> PORT 
+import Joi from 'joi';
+
+const schema = Joi.object().keys({
+    email:  Joi.string().email().required(),
+    password: Joi.string().regex(/^[a-Z0-9]{6-9}$/).required(),
+    paymentMethod: Joi.string().valid('mpesa', 'jenga').required(),
+})
+
 export function validatePassword(
     email: string, 
     password: string, 
     paymentMethod: string) 
 {
+    const result = Joi.validate({ email, password, paymentMethod}, schema)
+    console.log({ result })
+    if(result.error) {
+        console.log({ err: result.error })
+        //return error as response back to the client
+        return
+    } 
+
+    // if there is no error 
+    // -> send to either the jenga or mpesa queue 
+
+
    // if not 3 
    // return 400 bad request
    // email -> 400
