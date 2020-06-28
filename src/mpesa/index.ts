7b0ee308cc78decb7d2338a55dcd24fea22ef098
ezkemboi <ezrqnkemboi@gmail.com>
2020-06-28 12:05:04 +0300

    remove unused files

--- src/mpesa/index.ts
+++ src/mpesa/index.ts
@@ -1,29 +1,4 @@
 // PORT 8001
-
-const validMpesaRes = [
-    {
-        mpesaCode: 'NXQUIOAKSDII12JS',
-        mpesaUser: 'kemboi',
-        phoneNumber: '0722228902',
-        success: true,
-        amount: '5000',
-        message: 'Successfully paid 5000 for rent'
-    },
-    {
-        mpesaUser: 'Kebenei',
-        phoneNumber: '0798862345',
-        success: false,
-        amount: '5000',
-        message: 'Failed. Insufficient funds.'
-    }
-]
-
-export function mpesa() {
-    // process payment
-    // response -> send back to queue
-    console.log({ validMpesaRes })
-}
-
 import express from "express";
 import RabitMq from "./config/rabbitMq";
 import firebase from "./config/firebase";
